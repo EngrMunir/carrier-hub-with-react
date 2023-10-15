53adf0ac5eac57c1a45e123dd47ccd132ca6fcde
Md Sirajul Munir <abc@gmail.com>
2023-10-15 13:31:03 +0600

    applied jos are stored in local database

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -1,5 +1,7 @@
 import { useLoaderData, useParams } from "react-router-dom";
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { saveJobApplication } from "../Utility/localStorage";
 
 const JobDetails = () => {
     const jobs = useLoaderData();
@@ -7,18 +9,28 @@ const JobDetails = () => {
     const idInt = parseInt(id);
     const job = jobs.find(job => job.id === idInt)
     console.log(id, job);
+
+    const handleApplyJobs =()=>{
+        saveJobApplication(id);
+        toast('You have applied successfully');
+    }
     return (
         <div>
-            <h2>Job Details of: {id}</h2>
+            
             <div className="grid gap-4 md:grid-cols-4">
                 <div className="border md:col-span-3">
-
+                    <h2 className="text-4xl">Details Coming Here</h2>
+                    <h2>Job Details of: {job.job_title}</h2>
+                    <p>{job.company_name}</p>
                 </div>
                 <div className="border">
-
+                    <h2 className="text-2xl">side things</h2>
+                    <button onClick={handleApplyJobs} className="btn btn-primary w-full">Apply Now</button>
                 </div>
 
             </div>
+
+            <ToastContainer />
         </div>
     );
 };
